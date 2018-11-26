e3fba4983ed5780c5158479732e6ce6e6947ca8f
Makdir <38258574+Makdir@users.noreply.github.com>
2018-11-26 15:47:01 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,109 +1,191 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sqflite/sqflite.dart';
 
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(
+      new MaterialApp(
+        home: new MainScreen(),
+        routes: <String, WidgetBuilder> {
+          '/logout': (BuildContext context) => new LoginScreen(),
+          '/settings': (BuildContext context) => new SettingsScreen(),
+          '/route': (BuildContext context) => new RouteScreen(),
+          '/sync': (BuildContext context) => new SyncScreen(),
+        },
+      )
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
+    return new Scaffold( // 1
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text("Please, login"), // screen title
+
       ),
       body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
+            new Container(
+              padding: const EdgeInsets.all(16.0),
+              child: new TextField(
+                keyboardType: TextInputType.number,
+                //controller: _controller,
+                decoration: new InputDecoration(
+                  hintText: 'Zip Code',
+                ),
+                onSubmitted: (string) {
+                  return string;
+                },
+              ),
             ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+
+            new RaisedButton(onPressed:(){
+              button1(context);
+              } ,child: new Text("Go to Screen 2"),
+            )
           ],
         ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ) ,
     );
   }
+
+  void button1(BuildContext context){
+    print("Button 1");
+    Navigator.of(context).pushNamed('/screen2');
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+
+  TextField serverInputField;
+  
+  @override
+  Widget build(BuildContext context) {
+
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("Settings"),),
+      body: new Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+           serverInputField = new TextField(
+                                            decoration: InputDecoration(
+                                                labelText: 'Server address:'
+                                            ),
+          ),
+          // ignore: argument_type_not_assignable
+          RaisedButton(onPressed: saveData,
+
+          ),
+        ],
+
+      ) ,
+    );
+
+  }
+
+  void saveData() {
+    print(serverInputField.toString());
+    
+  }
+}
+
+class Screen3 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Screen 3"),
+
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new RaisedButton(onPressed:(){
+              button3(context);
+            } ,child: new Text("Back to Screen 1"),)
+          ],
+        ),
+      ) ,
+    );
+
+  }
+
+  void button3(BuildContext context){
+    print("Button 3");
+    Navigator.of(context).pop(true);
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(backgroundColor: Colors.brown),
+        body: new Container(
+        color: Colors.black38,
+        child: new ListView(
+            padding: const EdgeInsets.all(16.0),
+            children: <Widget>[
+              OutlineButton(
+                  onPressed: () { Navigator.of(context).pushNamed('/sync');},
+                  color: Colors.orange,
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(children: <Widget>[Icon(Icons.import_export),Text("Выполнить синхронизацию")],),
+                ),
+              OutlineButton(
+                  onPressed: () { Navigator.of(context).pushNamed('/route');},
+                  color: Colors.orange,
+                  padding: EdgeInsets.all(16.0),
+                  child: Row(children: <Widget>[Icon(Icons.list),Text("Маршрут")],),
+                ),
+              OutlineButton(
+                onPressed: () { Navigator.of(context).pushNamed('/settings');},
+                color: Colors.orange,
+                padding: EdgeInsets.all(16.0),
+                child: Row(children: <Widget>[Icon(Icons.settings),Text("Настройки")],),),
+            ],
+        )
+
+      )
+    );
+  }
+
+}
+
+class SyncScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: new Text("Синхронизация"),),
+        body: new Container(
+            color: Colors.deepPurple,
+            child: new RaisedButton(onPressed:(){ Navigator.of(context).pushNamed('/screen3'); }),
+
+            )
+        );
+     }
+  void syncButton(){
+
+  }
+}
+
+class RouteScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(title: new Text("Маршрут"),),
+        body: new Container(         )
+    );
+  }
+
 }
