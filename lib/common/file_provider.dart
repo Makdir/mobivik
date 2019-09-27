776217649fa71deb3abe5b04000fc1e8a2375afa
Kirill Shamrayev <makdir@gmail.com>
2019-09-27 17:30:32 +0300

    class FileProvider

--- lib/common/file_provider.dart
+++ lib/common/file_provider.dart
@@ -6,7 +6,24 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class FileProvider {
 
+  static openFile(String fileName) async{
+    Directory tempDir = await getTemporaryDirectory();
+    String tempPath = tempDir.path;
 
+    Directory outputDir = Directory(tempPath+Platform.pathSeparator+"input");
+    bool isExist = await outputDir.exists();
+    if (isExist==false) {
+      await outputDir.create(recursive: true);
+    }
+
+    File inputFile = new File(outputDir.path+Platform.pathSeparator+"$fileName.mv");
+
+    isExist = await inputFile.exists();
+    if (isExist==false) {
+      await inputFile.create(recursive: true);
+    }
+    return inputFile;
+  }
 
   static saveFile(String fileName) async{
     Directory tempDir = await getTemporaryDirectory();
@@ -24,7 +41,6 @@ class FileProvider {
     if (isExist==false) {
       await outputFile.create(recursive: true);
     }
-
     return outputFile;
   }
 
