c984ddf1b17d9cd9ae4963c9c288f6828823a2d8
Makdir <makdir@gmail.com>
2020-07-24 17:01:31 +0300

    The sales history in buy order done.

--- lib/fragments/sales_history.dart
+++ lib/fragments/sales_history.dart
@@ -35,16 +35,16 @@ class _SalesHistoryFragmentState extends State{
   getData() async {
     File salesFile = await FileProvider.openInputFile('sales');
     String content = await salesFile.readAsString();
-    print('content =  $content');
+    //print('content =  $content');
     try{
         salesList = await json.decode(content);
     }catch (e) {
         print('Wrong content of sales history.');
     }
-    print('salesList =  $salesList');
+    //print('salesList =  $salesList');
     String outletId = outlet.id;
     salesList =  salesList.where((element) => element['id']==outletId).toList();
-    print('2 salesList =  $salesList');
+    //print('2 salesList =  $salesList');
     setState(() {
 
     });
@@ -53,22 +53,57 @@ class _SalesHistoryFragmentState extends State{
 
   @override
   Widget build(BuildContext context) {
+    //double iconSize = 40;
     return ListView.builder(
       padding: EdgeInsets.all(1.0),
       itemCount: salesList.length,
       itemBuilder: (BuildContext context, int index) {
         return Card(
           child: ListTile(
-            //trailing: Icon(Icons.arrow_forward_ios),
             title:Text(salesList[index]['doc']),
-            subtitle: Text(salesList[index]['table'].toString()),
+            subtitle: HistorySalesTable(salesList[index]['table']),
           ),
         );
       },
     );
   }
 
+  Widget HistorySalesTable(List saleDoc){
+    TextStyle headerTextStyle = TextStyle(color: Colors.black, fontWeight: FontWeight.w500);
+    TextStyle rowTextStyle = TextStyle(color: Colors.black);
+    List<TableRow> sales = [];
+    sales.add(
+        TableRow(
+          decoration: BoxDecoration(color: Colors.grey),
+            children: [
+              Text('Товар', style: headerTextStyle, textAlign: TextAlign.center,),
+              Text('Кол-во', style: headerTextStyle, textAlign: TextAlign.center,),
+              Text('Ед. изм.', style: headerTextStyle, textAlign: TextAlign.center,),
+              Text('Цена', style: headerTextStyle, textAlign: TextAlign.center,),
+            ]
+        )
+    );
 
+    for(Map sale in saleDoc) {
+      String price = double.parse(sale['price'].toString()).toStringAsFixed(2);
+      sales.add(
+          TableRow(
+              children: [
+                Text(' ${sale['name']}', style: rowTextStyle, textAlign: TextAlign.start),
+                Text(' ${sale['number']} ', style: rowTextStyle, textAlign: TextAlign.center),
+                Text(' ${sale['unit']}',  style: rowTextStyle, textAlign: TextAlign.start),
+                Text(' $price', style: rowTextStyle, textAlign: TextAlign.start),
+              ],
+          )
+      );
+    }
+
+    return Table(
+      border: TableBorder.all(),
+      defaultColumnWidth: IntrinsicColumnWidth(),
+      children: sales,
+    );
+  }
 
 
 }
